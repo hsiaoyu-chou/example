b9b981a387b8cff43d35c6bef8ee81144fa896fd
Hsiao-Yu Chou <littlefishchou.cs03g@g2.nctu.edu.tw>
2018-03-05 17:55:40 +0800

    reproduce a issue (order for sections and components if they are in the same section)

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,7 +1,36 @@
 module.exports = {
 	title: 'React Style Guide Example',
 	defaultExample: true,
-	components: './lib/components/**/[A-Z]*.js',
+	ignore: [
+		'**/index.js'
+	],
+	sections: [
+		{
+			name: 'Buttons',
+			sections: [
+				{
+					name: 'Buttons',
+					content: './lib/components/Buttons/Button/Readme.md',
+					components: './lib/components/Buttons/Button/Button.js'
+				},
+				{
+					name: 'PushButton',
+					components: './lib/components/Buttons/PushButton/PushButton.js'
+				},
+			],
+			components: './lib/components/Buttons/*.js'
+		},
+		{
+			name: 'Modal',
+			content: './lib/components/Modal/Readme.md',
+			components: './lib/components/Modal/Modal.js'
+		},
+		{
+			name: 'Placeholder',
+			content: './lib/components/Placeholder/Readme.md',
+			components: './lib/components/Modal/Placeholder.js'
+		}
+	],
 	webpackConfig: {
 		module: {
 			loaders: [
